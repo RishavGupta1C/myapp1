94cd36fe2a6a1c9d943d8a1b1be90be4eb95eab4
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-08-28 23:57:08 +0530

    commit_2

--- lib/gradient_constraint.dart
+++ lib/gradient_constraint.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:myapp1/text_style.dart';
+
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-class GradientContainer extends StatefulWidget {
+class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
   final Color color3;
-  var activeDiceImage = 'assets/images/dice-1.png';
+  
 
   // required is needed for named arguments
-  GradientContainer(this.color1, this.color2, this.color3, {Key? key})
+  const GradientContainer(this.color1, this.color2, this.color3, {Key? key})
       : super(key: key);
 
   // Alternative Constructor
   // factory GradientContainer.red(Color color1) {
   //   return GradientContainer(color1, Colors.transparent, Colors.transparent)
   // }
-  GradientContainer.rgb({Key? key})
+  const GradientContainer.rgb({Key? key})
       : color1 = Colors.redAccent,
         color2 = Colors.blueAccent,
         color3 = Colors.greenAccent,
@@ -35,30 +35,12 @@ class GradientContainer extends StatefulWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(activeDiceImage, width: 200),
-            // Sized Box is a special widget which has a fixed width and height
-            // Unlike others whose size depends on the content inside
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(left: 10, right: 10),
-                backgroundColor: Colors.green,
-              ),
-              child: const text_style('Roll Dice'),
-            ),
-          ],
-        ),
+        child: 
       ),
     );
   }
 
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-3.png';
-  }
+  
 }
 
 
